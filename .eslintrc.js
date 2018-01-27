eab7d08e20b2484f1744c8f2c433013285b592e1
Mathieu <mathieux51@gmail.com>
2018-01-27 17:45:36 +0100

    Add eslint and gitignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,4 +11,4 @@ module.exports = {
     quotes: ["error", "single"],
     semi: ["error", "never"]
   }
-}
+};
--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-const http = require("http");
+const http = require('http')
 
 http
-  .createServer((request, response) => {
-    if (request.method === "GET" && request.url === "/") {
-      response.setHeader("Content-Type", "application/json");
-      response.end(JSON.stringify({ message: "test" }, null, 2));
-    } else if (request.method === "POST" && request.url === "/") {
-      let body = [];
-      request
-        .on("data", chunk => {
-          body.push(chunk);
-        })
-        .on("end", () => {
-          body = Buffer.concat(body).toString();
-          response.end(body);
-        });
-    } else {
-      response.statusCode = 404;
-      response.end();
-    }
+  .createServer((req, res) => {
+    res.statusCode(200)
+    res.end()
   })
-  .listen(8080);
+  .listen(8080)
