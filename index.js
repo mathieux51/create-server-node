c6b7b5cfcd9d65c6987e4c01073e9528aad0db50
Mathieu <mathieux51@gmail.com>
2018-01-28 16:43:57 +0100

    Add index.html response

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const http = require('http')
-
-const data = {
-  message: 'Hello World'
-}
+const fs = require('fs')
 
 http
   .createServer((req, res) => {
-    res.setHeader('Content-Type', 'application/json')
-    res.end(JSON.stringify(data))
+    if (req.method === 'GET' && req.url === '/') {
+      fs.readFile('index.html', 'utf8', (err, data) => {
+        if (err) throw err
+        res.end(data)
+      })
+    }
   })
   .listen(8080)
